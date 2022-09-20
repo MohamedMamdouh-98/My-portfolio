f6bc76a1f612c23b67fa0d2e0e7f78a03cdcdcf5
MohamedMamdouh-98 <miido96@gmail.com>
2022-09-20 14:21:01 +0200

    [deleted]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import Board from "./components/board/Board";
-import Start from "./components/start/Start";
 import './index.css'
 const App = () => {
   return (
     <div className="container App">
-      <Start />
-      <Board />
+      
     </div>
   );
 };
